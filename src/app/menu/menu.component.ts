d48a329426febe81fd4935e6663907d11a9a77dc
c1297721 <c1297721@bb.com.br>
2019-10-14 19:39:35 -0300

    implementacao layout munu lateral bootstrap

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+    selector: 'menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
-  public texto: string
+    home: string = '';
+    anotacoes: string = '';
+    login: string = '';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-    this.texto = "angular notas"
-  }
+    removeActive() {
+        this.home = '';
+        this.anotacoes = '';
+        this.login = '';
+    }
 
+    menuActive(active: any) {
+        this.removeActive();
+
+        switch (active) {
+            case 'home':
+                this.home = 'active';
+                break;
+            case 'anotacoes':
+                this.anotacoes = 'active';
+                break;
+            case 'login':
+                this.login = 'active';
+                break;
+            default:
+                break;
+        }
+    }
+
+    initActive() {
+        if (window.location.hash == '#/home') {
+            this.home = 'active';
+        }
+        if (window.location.hash == '#/anotacoes') {
+            this.anotacoes = 'active';
+        }
+        if (window.location.hash == '#/login') {
+            this.login = 'active';
+        }
+    }
+
+    ngOnInit() {
+        this.initActive();
+    }
 }
