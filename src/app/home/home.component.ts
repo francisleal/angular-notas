11a4d218f2a8e287ffe8717c6b325a600abbc93a
c1297721 <c1297721@bb.com.br>
2019-10-22 17:29:19 -0300

    criacao dashboard

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NotasService } from '../notas.service';
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    public graficoLinksLabels = ['Links'];
+    public graficoLinksData: number[] = [];
+    public graficoLinksType = 'doughnut';
+    public graficoColor = [{ backgroundColor: ['#007bff'] }];
 
-  ngOnInit() {
-  }
+    public doughnutChartLabels = ['undefined'];
+    public doughnutChartData = [100, 50];
+    public doughnutChartType = 'doughnut';
+    public doughnutChartColor = [{ backgroundColor: ['rgba(0,0,255,0.3)'] }];
+
+    constructor(private notasService: NotasService) { }
+
+    ngOnInit() {
+        this.graficoLinks()
+    }
+
+    public graficoLinks() {
+        this.notasService.getLinks().subscribe(
+            sucesso => this.graficoLinksData = [sucesso.length, (100 - sucesso.length)]
+        )
+    }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
 
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ChartsModule
   ]
 })
 export class HomeModule { }
